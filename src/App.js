b0dbef4278d867d848335397df4068286e139594
Vrajesh Rana <vur2@njit.edu>
2020-12-19 23:07:08 -0500

    random updates idk where from

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Content from "./components/Content";
-import Footer from './components/Footer'
+import Footer from "./components/Footer";
 
 const App = () => {
-  return (
-    <div className="App">
-        <Header />
-        <Content />
-        <Footer />
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
